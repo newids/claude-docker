f25e8f81b7b248cc210251180519d6a5478fe8e6
최진석 <newids@gmail.com>
2026-08-09 15:08:25 +0900

    setup.sh 에 git config 관련 내용 추가

--- setup.sh
+++ setup.sh
@@ -7,5 +7,9 @@ mv keys/* ~/.ssh
 rm -rf keys/
 
 git --version
+git config --global user.email "newids@gmail.com"
+git config --global user.name "Jeanseok Choi"
+git config --global init.defaultBranch main
 
 source ~/.zshrc
+
