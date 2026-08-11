b191facf0daf88d32ff2b32e761374cb45d4ba60
Jeanseok Choi <newids@gmail.com>
2026-08-11 14:31:31 +0900

    setup.sh에 설치 스크립트 추가 및 zshrc 업데이트

--- setup.sh
+++ setup.sh
@@ -11,5 +11,9 @@ git config --global user.email "newids@gmail.com"
 git config --global user.name "Jeanseok Choi"
 git config --global init.defaultBranch main
 
-source ~/.zshrc
 
+curl -fsSL https://claude.ai/install.sh | bash
+
+echo "\n\tclaude --dangerously-skip-permissions\n\n"
+
+echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.zshrc && source ~/.zshrc
